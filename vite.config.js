6cef3eb8ec15ef991a3286f951385a974b3a8fdd
Sofiia Alekhina <grandpoopris@yandex.ru>
2025-04-01 16:13:40 +0700

    fix map

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: '/genius-coin/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
-  }
+  },
+  plugins: [
+    Sitemap({ hostname: 'https://genius-coin.com' }),
+  ],
 })
